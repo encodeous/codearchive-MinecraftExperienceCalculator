25410cba59c4f1ad42ecf38b86497e5dc67fbaa7
encodeous <47889571+encodeous@users.noreply.github.com>
2019-12-29 19:46:21 -0500

    Add files via upload

---
+++ ExpCalc/CalculateEXP.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace ExpCalc
+{
+    class CalculateEXP
+    {
+        public static BigInteger getExpAtLevel(BigInteger level)
+        {
+            if (level <= 15)
+            {
+                return (2 * level) + 7;
+            }
+            if ((level >= 16) && (level <= 30))
+            {
+                return (5 * level) - 38;
+            }
+            return (9 * level) - 158;
+        }
+
+        public static BigInteger getExpToLevel(BigInteger level)
+        {
+            BigInteger currentLevel = 0;
+            BigInteger exp = 0;
+
+            while (currentLevel < level)
+            {
+                exp = exp + getExpAtLevel(currentLevel);
+                currentLevel++;
+            }
+            if (exp < 0)
+            {
+                exp = int.MaxValue;
+            }
+            return exp;
+        }
+        public static BigInteger getTotalLevels(BigInteger exp, out BigInteger remainder)
+        {
+            BigInteger timesLooped = 0;
+            BigInteger currentExp = exp;
+            BigInteger Level = 0;
+            BigInteger PrevExp = 0;
+            while (exp >= 0)
+            {
+                PrevExp = exp;
+                exp -= getExpAtLevel(Level);
+                Level++;
+            }
+            remainder = getExpAtLevel(Level) - PrevExp;
+            return Level-1;
+        }
+
+        public static BigInteger getTotalExperience(BigInteger level)
+        {
+            BigInteger exp = (getExpAtLevel(level) * (BigInteger)getExpPercentNeeded(level));
+            BigInteger currentLevel = level;
+            while (currentLevel > 0)
+            {
+                currentLevel--;
+                exp += getExpAtLevel(currentLevel);
+            }
+            if (exp < 0)
+            {
+                exp = int.MaxValue;
+            }
+            return exp;
+        }
+        
+        public static BigInteger getExpUntilNextLevel(BigInteger exp, BigInteger NextLevel)
+        {
+            return getTotalExperience(NextLevel) - exp;
+        }
+        
+        public static double getExpPercentNeeded(BigInteger level)
+        {
+            BigInteger temp = getExpAtLevel(level+1);
+            return (double)getExpAtLevel(level) / (double)temp;
+        }
+    }
+}
---
+++ ExpCalc/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExpCalc
+{
+    public partial class Form1 : Form
+    {
+        int CurrentExp = 0;
+        int TargetExp = 0;
+        bool CurrentLevel = false;
+        bool TargetLevel = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int exp;
+            int.TryParse(textBox1.Text, out exp);
+            textBox3.Text = "" + CalculateEXP.getTotalExperience(exp);
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            BigInteger exp;
+            BigInteger.TryParse(textBox2.Text, out exp);
+            BigInteger remainder = 0;
+            BigInteger expToLevel = CalculateEXP.getTotalLevels(exp, out remainder);
+            Debug.WriteLine(remainder);
+            textBox4.Text = "" + expToLevel;
+            textBox5.Text = "" + CalculateEXP.getExpUntilNextLevel(exp, expToLevel+1);
+        }
+
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
